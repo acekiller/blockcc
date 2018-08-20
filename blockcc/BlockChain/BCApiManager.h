7957fb697634315835c6b0a17df9719ab5ea6dc1
acekiller <fengxijun51020@hotmail.com>
2018-08-21 07:16:49 +0800

    add api manager

---
+++ blockcc/BlockChain/BCApiManager.h
@@ -0,0 +1,13 @@
+//
+//  BCApiManager.h
+//  blockcc
+//
+//  Created by acekiller on 2018/8/21.
+//  Copyright © 2018年 acekiller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCApiManager : NSObject
+
+@end
---
+++ blockcc/BlockChain/BCApiManager.m
@@ -0,0 +1,13 @@
+//
+//  BCApiManager.m
+//  blockcc
+//
+//  Created by acekiller on 2018/8/21.
+//  Copyright © 2018年 acekiller. All rights reserved.
+//
+
+#import "BCApiManager.h"
+
+@implementation BCApiManager
+
+@end
