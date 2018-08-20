27d9e46344e33e82349d3129abc3b26d8603ef63
acekiller <fengxijun51020@hotmail.com>
2018-08-21 06:57:26 +0800

    add .gitignore file

---
+++ blockcc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blockcc
+//
+//  Created by acekiller on 2018/8/21.
+//  Copyright © 2018年 acekiller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blockcc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  blockcc
+//
+//  Created by acekiller on 2018/8/21.
+//  Copyright © 2018年 acekiller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
